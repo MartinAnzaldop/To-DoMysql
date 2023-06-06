be9b166d61c1b9f4fadd073b7a174bb844945145
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-06-05 19:41:59 -0600

    base de datos restablecida

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ import { createPool } from "mysql2/promise";
 import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE} from "./config.js";
 
 export const pool = createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_DATABASE,
+  host: 'containers-us-west-42.railway.app',
+  user: 'root',
+  password:'WwReoNGB80YZqUF9ybZS',
+  port: 6178,
+  database:'railway'
 });
 
