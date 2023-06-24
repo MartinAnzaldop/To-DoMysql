fd9d1ae8b981ca3c51912e61ed8d914f442e375a
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-06-24 15:47:15 -0600

    base de datos cambiada 240623

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ import { createPool } from "mysql2/promise";
 import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE} from "./config.js";
 
 export const pool = createPool({
-  host: 'containers-us-west-42.railway.app',
+  host: 'containers-us-west-10.railway.app',
   user: 'root',
-  password:'WwReoNGB80YZqUF9ybZS',
-  port: 6178,
+  password:'kTdUK6Kn8AjGUPgnTUNW',
+  port: 7798,
   database:'railway'
 });
 
